1b9865daf0130b5dc84bda223cd7df4f7ad3211c
Mohammad-Ishaq98 <mdishaqibrohim@gmail.com>
2022-01-03 22:58:46 +0600

    Video Player using js

---
+++ app.js
@@ -0,0 +1,23 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener("click", function(){
+  !btn.classList.contains("slide") ? (btn.classList.add("slide"), video.pause()): (btn.classList.remove("slide"), video.play());
+  // if(!btn.classList.contains('slide')){
+  //   btn.classList.add('slide')
+  // }
+  // else{
+  //   btn.classList.remmove('slide')
+  // }
+})
+
+//preloader
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', ()=>{
+  let setPreloader = preloader.classList.add('hide-preloader');
+  document.querySelector('.hide-preloader').style.transition = "2.5s"
+})
